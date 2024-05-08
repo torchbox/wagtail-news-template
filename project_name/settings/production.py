1e23d987a8d6ffa06a88e30e1e466e6ee28aa89b
Jake Howard <jake.howard@torchbox.com>
2024-05-08 16:20:46 +0100

    Remove secret key stub

--- project_name/settings/production.py
+++ project_name/settings/production.py
@@ -1,19 +1,3 @@
-import os
-import random
-import string
-
-from .base import *
+from .base import *  # noqa
 
 DEBUG = False
-
-# DJANGO_SECRET_KEY *should* be specified in the environment. If it's not, generate an ephemeral key.
-if "DJANGO_SECRET_KEY" in os.environ:
-    SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
-else:
-    # Use if/else rather than a default value to avoid calculating this if we don't need it
-    print(  # noqa: T201
-        "WARNING: DJANGO_SECRET_KEY not found in os.environ. Generating ephemeral SECRET_KEY."
-    )
-    SECRET_KEY = "".join(
-        [random.SystemRandom().choice(string.printable) for i in range(50)]
-    )
