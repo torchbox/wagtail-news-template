b3edfe90ddd48cec743cf0ad45e0692b0b5f9f9c
Jake Howard <jake.howard@torchbox.com>
2024-05-22 10:44:56 +0100

    Ensure `MEDIA_ROOT` can be overridden

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -158,7 +158,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.environ.get("MEDIA_ROOT", os.path.join(BASE_DIR, "media"))
 MEDIA_URL = "/media/"
 
 # Default storage settings, with the staticfiles storage updated.
