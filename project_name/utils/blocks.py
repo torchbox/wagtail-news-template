36304d9c129e1cfe4cbac176774adbd893ed51e5
Meagen Voss <45881480+vossisboss@users.noreply.github.com>
2025-02-28 18:06:43 -0500

    Updated accordion block code.

--- project_name/utils/blocks.py
+++ project_name/utils/blocks.py
@@ -11,7 +11,7 @@ from wagtail.snippets.blocks import SnippetChooserBlock
 from {{ project_name }}.utils.struct_values import CardStructValue, LinkStructValue
 
 
-class AccordionBlock(blocks.StructBlock):
+class AccordionSectionBlock(blocks.StructBlock):
     title = blocks.CharBlock(max_length=255)
     content = blocks.RichTextBlock()
 
@@ -21,12 +21,18 @@ class AccordionBlock(blocks.StructBlock):
 
 
 class AccordionBlock(blocks.StructBlock):
-    heading = blocks.ListBlock(AccordionBlock())
-    list = blocks.ListBlock(AccordionBlock())
+    sections = blocks.ListBlock(
+        AccordionSectionBlock(),
+    )
 
     class Meta:
         icon = "list-ol"
-        template = "components/accordion/accordion.html"
+        template = "components/streamfield/blocks/accordion.html"
+
+    def get_context(self, value, parent_context=None):
+        ctx = super().get_context(value, parent_context=parent_context)
+        ctx["accordions"] = value["sections"]
+        return ctx
 
 
 class ImageBlock(blocks.StructBlock):
